a4428eac50ebab8eca0102efb138657be5363456
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-04-16 12:53:18 +0200

    feat: mimic the functionality of ch1 Controller but using xml instead of php oop

--- src/app/code/Pulsestorm/Nofrillslayout/Controller/Chapter2/Index.php
+++ src/app/code/Pulsestorm/Nofrillslayout/Controller/Chapter2/Index.php
@@ -23,7 +23,19 @@ class Index implements HttpGetActionInterface {
     {
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $layout        = $objectManager->get(\Pulsestorm\Nofrillslayout\Framework\view\Layout::class);
-        $layout->addContainer('top', 'The top level container');
+
+        $updateManager = $layout->getUpdate();
+        $updateManager->addUpdate('<container name="top"></container>');
+
+        /*
+            Important : The writer mentioned that we no longer need the generateElements() method, however my code didn't work without it .
+
+            The generateXml() seems to be optional (I commented it out and the code
+            still works)
+        */
+        // $layout->generateXml();
+        $layout->generateElements();
+
 
         $blockOne   = $layout->createBlock(
             'Magento\Framework\View\Element\Template',
@@ -41,7 +53,7 @@ class Index implements HttpGetActionInterface {
         $structure->setAsChild('pulsestorm_nofrills_chapter2_block1', 'top');
         $structure->setAsChild('pulsestorm_nofrills_chapter2_block2', 'top');
 
-        $layout->generateElements();
+        // $layout->generateElements();
         echo $layout->getOutput();
         exit;
     }
