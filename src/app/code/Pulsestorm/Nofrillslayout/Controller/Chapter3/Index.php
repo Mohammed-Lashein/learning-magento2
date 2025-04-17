55544459dbdfde20f8e7f6d4031dc7125969d00d
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-04-17 10:26:38 +0200

    feat: create and use our 1st custom layout handle

--- src/app/code/Pulsestorm/Nofrillslayout/Controller/Chapter3/Index.php
+++ src/app/code/Pulsestorm/Nofrillslayout/Controller/Chapter3/Index.php
@@ -1,17 +1,22 @@
 <?php
 
-// namespace declaration is important in order for the execute method to work
 namespace Pulsestorm\Nofrillslayout\Controller\Chapter3;
 use Pulsestorm\Nofrillslayout\Controller\Base;
 
 class Index extends Base {
-    // Important in order for the class to work
-    public function __construct(){}
+    protected $resultPageFactory;
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+    ){
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
     public function execute()
     {
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        // $layout  = $objectManager->get(\Pulsestorm\Nofrillslayout\Framework\view\Layout::class);
-        echo __METHOD__;
-        exit;
+        $pageObj = $this->resultPageFactory->create();
+        $pageObj->addHandle('our_1st_custom_handle');
+
+        return $pageObj;
     }
 }
