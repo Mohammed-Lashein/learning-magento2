088aabd73f66600775485ddbbe59b7658381c650
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-04-18 09:48:24 +0200

    refactor: add note about the ability to use getActionFullName() method even if the IDE doesn't recognize its definition

--- src/app/code/Pulsestorm/Nofrillslayout/Controller/Chapter3/Index.php
+++ src/app/code/Pulsestorm/Nofrillslayout/Controller/Chapter3/Index.php
@@ -24,9 +24,18 @@ class Index extends Base {
         // $pageObj->addHandle('our_1st_custom_handle');
 
         /* I can't find getFullActionName() either in the code or online . It seems that
-        it is deprecated however I couldn't find the release that deprecated it  */
-        var_dump($this->request->getActionName());
-        var_dump($this->request->getModuleName());
+        it is deprecated however I couldn't find the release that deprecated it .
+
+        MAJOR UPADTE : I found a call to the previously mentioned method in
+        \Magento\Framework\View\Result\Page.php, and also my IDE couldn't recognize its
+        definition .
+
+        I gave it a try (since it is used internally by magento and everything is working
+        ) and it worked !
+        */
+        // var_dump($this->request->getActionName());
+        // var_dump($this->request->getModuleName());
+        // var_dump($this->request->getFullActionName());
         return $pageObj;
     }
 }
